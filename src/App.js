b5afdaaba907d1d3b49727024fe75d58edb4f8aa
Palmy <nicha.satt@gmail.com>
2022-10-10 11:10:09 +0700

    feature-01

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Test feature-01 Palmy
+          Learn React Test feature-01 palmy
         </a>
       </header>
     </div>
